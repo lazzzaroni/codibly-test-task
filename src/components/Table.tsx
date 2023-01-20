b83d3bbbf55275effea64a3c147e706fef7ffd67
lazzzaroni <iamnotlazzaroni@gmail.com>
2023-01-20 17:17:48 +0100

    add modal & styling

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Data, ProductsPage } from "../interfaces";
 
 export const Table = ({ color }: { color: ProductsPage }) => {
@@ -22,12 +23,61 @@ export const Table = ({ color }: { color: ProductsPage }) => {
 };
 
 const Item = ({ item }: { item: Data }) => {
+  const [openModal, setOpenModal] = useState<boolean>(false);
+
   return (
-    <tr key={item.id} style={{ background: `${item.color}` }}>
-      <td>{item.id}</td>
-      <td>{item.name}</td>
-      <td>{item.year}</td>
-    </tr>
+    <>
+      <tr
+        onClick={() => setOpenModal(true)}
+        key={item.id}
+        style={{ background: `${item.color}` }}
+      >
+        <td>{item.id}</td>
+        <td>
+          {item.name.replace(/(^\w{1})|(\s+\w{1})/g, (letter) =>
+            letter.toUpperCase()
+          )}
+        </td>
+        <td>{item.year}</td>
+      </tr>
+      {openModal && <Modal item={item} setOpenModal={setOpenModal} />}
+    </>
+  );
+};
+
+const Modal = ({
+  item,
+  setOpenModal,
+}: {
+  item: Data;
+  setOpenModal: React.Dispatch<React.SetStateAction<boolean>>;
+}) => {
+  return (
+    <div className="modalBackground">
+      <div className="modalContainer">
+        <div
+          className="modalColorBox"
+          style={{ background: `${item.color}`, color: "#fff" }}
+        >
+          <h2>Color Of The Year</h2>
+          <h1>{item.year}</h1>
+        </div>
+        <div className="modalDescription">
+          <p>Pantone {item.pantone_value}</p>
+          <p>
+            {item.name.replace(/(^\w{1})|(\s+\w{1})/g, (letter) =>
+              letter.toUpperCase()
+            )}
+          </p>
+          <button
+            className="modalCloseButton"
+            onClick={() => setOpenModal(false)}
+          >
+            Close
+          </button>
+        </div>
+      </div>
+    </div>
   );
 };
 
